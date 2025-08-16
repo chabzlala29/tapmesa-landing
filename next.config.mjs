66f271cc27da638cad605df44448d404b18df0e9
Charles Pobre <charlesdarwin.pobre@frollo.us>
2025-08-16 18:41:57 +0800

    removed export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Enables static HTML export (replaces `next export`)
+  basePath: '/tapmesa-landing', // Replace with your actual repo name
+  assetPrefix: '/tapmesa-landing/',
+  trailingSlash: true,
+};
 
 export default nextConfig;
